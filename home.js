bc6673bba17293632036c3ade466eee8b1949c43
mzwandile-stuurman <stuurmanmzwandile@gmail.com>
2024-04-05 15:01:27 +0200

    accordion change

--- home.js
+++ home.js
@@ -1,9 +1,8 @@
-var date = new Date();
+var date = new Date('2024-04-04T15:30:00').toLocaleDateString();
 
 var Today = document.getElementsByClassName("expire");
 
 for (let index = 0; index < Today.length; index++) {
-    Today[index].innerHTML = date.getFullYear();
+    Today[index].innerHTML =  "Date posted" +" "+ date;
 
-    
 }
