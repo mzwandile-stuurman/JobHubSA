df2204ae5e8749943fa8a0068019a48277770117
mzwandile-stuurman <stuurmanmzwandile@gmail.com>
2024-04-19 11:38:54 +0200

    adding more jobs

--- home.js
+++
@@ -1,8 +0,0 @@
-var date = new Date('2024-04-04T15:30:00').toLocaleDateString();
-
-var Today = document.getElementsByClassName("expire");
-
-for (let index = 0; index < Today.length; index++) {
-    Today[index].innerHTML =  "Date posted" +" "+ date;
-
-}
---
+++ javascript/home.js
@@ -0,0 +1,7 @@
+var date = new Date().toLocaleDateString();
+
+var Today = document.getElementById("today");
+
+Today.innerHTML = date;
+
+
